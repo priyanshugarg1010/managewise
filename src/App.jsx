443f2b6cbf2925d8170bd75321869297c066ad19
Priyanshu Garg <priyanshugarg1810@gmail.com>
2024-02-08 11:25:47 +0530

    add the github link

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,28 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Lottie from "lottie-react";
+import github from "../public/github.json";
 
 const App = () => {
   const Layout = () => {
     return (
       <div className="app font-[inter] sm:min-w-[]  md:w-[px] lg:w-[100%] w-auto">
         <Navbar />
+        <div className="fixed bottom-0 z-50 right-0">
+          <a
+            href="https://github.com/priyanshugarg1010/managewise"
+            target="blank"
+          >
+            <Lottie
+              animationData={github}
+              style={{ height: "100px" }}
+              loop={true}
+              height={10}
+              width={10}
+            />
+          </a>
+        </div>
         <Outlet />
         <Footer />
       </div>
